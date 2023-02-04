36e44d4be6311bfb636563de480a731d1e4f09b0
eugen-adesso-mobile <eugen-alexander.habrony@adesso-mobile.de>
2023-02-04 01:38:18 +0100

    Added User model and controller

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ connectDB().then();
 // Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 
 
@@ -38,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5080;
