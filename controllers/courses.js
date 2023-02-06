7c417df3891f76b542531dfbd8ecc581b2e79062
eugen-adesso-mobile <eugen-alexander.habrony@adesso-mobile.de>
2023-02-07 00:10:15 +0100

    Added authorization to courses

--- controllers/courses.js
+++ controllers/courses.js
@@ -51,6 +51,7 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @access	Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
 	req.body.bootcamp = req.params.bootcampId;
+	req.body.user = req.user.id;
 
 	const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
@@ -59,6 +60,11 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
 			new ErrorResponse(`No bootcamp with id of ${req.params.bootcampId}`),
 			404
 		);
+	}
+
+	// Make sure user is owner
+	if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		return next(new ErrorResponse(`User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`, 401));
 	}
 
 	const course = await Course.create(req.body);
@@ -82,7 +88,12 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
 		);
 	}
 
-	console.log(req.body);
+
+	// Make sure user is course owner
+	if(course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		return next(new ErrorResponse(`User ${req.user.id} is not authorized to update course to bootcamp ${course._id}`, 401));
+	}
+
 	course = await Course.findByIdAndUpdate(req.params.id, req.body, {
 		new: true,
 		runValidators: true
@@ -106,6 +117,11 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
 			new ErrorResponse(`No course with id of ${req.params.id}`),
 			404
 		);
+	}
+
+	// Make sure user is course owner
+	if(course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		return next(new ErrorResponse(`User ${req.user.id} is not authorized to delete course to bootcamp ${course._id}`, 401));
 	}
 
 	await course.remove();
