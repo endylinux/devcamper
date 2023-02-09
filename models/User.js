b1bee4be8b1b72f692738eadd7c98b29470cd72f
eugen-adesso-mobile <eugen-alexander.habrony@adesso-mobile.de>
2023-02-09 14:26:35 +0100

    Added reset password

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {raw} = require("express");
 
 const UserSchema = new mongoose.Schema({
 	name: {
@@ -14,7 +13,7 @@ const UserSchema = new mongoose.Schema({
 		required: [true, 'Please add an email'],
 		unique: true,
 		match: [
-			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+			/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
 			'Please add a valid email'
 		]
 	},
@@ -26,7 +25,7 @@ const UserSchema = new mongoose.Schema({
 	password: {
 		type: String,
 		required: [true, 'Please add a password'],
-		minLength: 6,
+		minlength: 6,
 		select: false
 	},
 	resetPasswordToken: String,
@@ -37,9 +36,9 @@ const UserSchema = new mongoose.Schema({
 	}
 });
 
-// Encrypting password using bcrypt
+// Encrypt password using bcrypt
 UserSchema.pre('save', async function(next) {
-	if(!this.isModified('password')) {
+	if (!this.isModified('password')) {
 		next();
 	}
 
@@ -48,24 +47,27 @@ UserSchema.pre('save', async function(next) {
 });
 
 // Sign JWT and return
-UserSchema.methods.getSignedJwtToken = function () {
+UserSchema.methods.getSignedJwtToken = function() {
 	return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
 		expiresIn: process.env.JWT_EXPIRE
 	});
-}
+};
 
 // Match user entered password to hashed password in database
-UserSchema.methods.matchPassword = async function (enteredPassword) {
+UserSchema.methods.matchPassword = async function(enteredPassword) {
 	return await bcrypt.compare(enteredPassword, this.password);
-}
+};
 
 // Generate and hash password token
-UserSchema.methods.getResetPasswordToken = function () {
+UserSchema.methods.getResetPasswordToken = function() {
 	// Generate token
 	const resetToken = crypto.randomBytes(20).toString('hex');
 
 	// Hash token and set to resetPasswordToken field
-	this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+	this.resetPasswordToken = crypto
+		.createHash('sha256')
+		.update(resetToken)
+		.digest('hex');
 
 	// Set expire
 	this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
